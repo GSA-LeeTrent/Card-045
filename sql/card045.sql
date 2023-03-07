5d999b988d7bd8e048a537f4fdfcf8cc978aae07
LeeTTrent <7557877312@GSA.GOV>
2023-03-07 11:34:47 -0600

    working on card 45

--- sql/card045.sql
+++ sql/card045.sql
@@ -3,3 +3,8 @@ FROM new_hrdw.nhrdw_qms_notifications_current_v
 GROUP BY qms_error_code;
 
 SELECT * FROM  new_hrdw.nhrdw_qms_notifications_current_v;
+
+
+-- SELECT DISTINCT udf_field_label FROM hiring.mgs_mstr_adhoc_g_udf;
+
+SELECT udf_field_label, udf_field_value FROM hiring.mgs_mstr_adhoc_g_udf WHERE udf_field_label = 'HR Spec Assigned Branch';
